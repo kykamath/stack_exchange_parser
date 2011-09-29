8ea331c01453b22c45f8c5e0d2471e102109aa06
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-29 16:11:27 -0500

    modified parser

--- parser.py
+++ parser.py
@@ -4,7 +4,7 @@ Created on Sep 29, 2011
 @author: kykamath
 '''
 from lxml import etree
-import dateutil.parser, os
+import dateutil.parser, os, cjson, glob
 from library.file_io import FileIO
 
 current_data_path = '/mnt/chevron/kykamath/data/stack_exchange/Content/android_enthusiasts/'
@@ -30,22 +30,6 @@ def iteratePosts(data_path, rowType=POST, interstedKeys = 'Id PostTypeId ParentI
 
 def iterateComments(data_path, rowType=COMMENT,interstedKeys = 'Id PostId CreationDate UserId'.split()):
     for l in iterateRows(data_path+'comments.xml', rowType, interstedKeys): yield l
-    
-def createOutputFileFor(current_data_path, iterators):
-    numberOfSplit = 10
-    totalLines = 0
-    tempDir = '%s/temp/'%current_data_path
-    allDataFile = tempDir+'data.txt'
-    if not os.path.exists(tempDir):  os.umask(0), os.makedirs(tempDir, 0777)
-    for iterator in iterators:
-        for data in iterator: 
-            totalLines+=1
-            FileIO.writeToFileAsJson(data, allDataFile)
-    linesPerFile = totalLines/numberOfSplit
-    os.chdir(tempDir)
-    os.system('split -l %s %s'%(linesPerFile, allDataFile))
-    
-    
 
 class Iterator:
         def __init__(self, iterator): 
@@ -62,8 +46,39 @@ def iterateDataOrderedByTime(iterators):
         nextIteratorWithSmallestValue = min(iterators, key=lambda it: it.current['CreationDate'])
         yield nextIteratorWithSmallestValue.current
         if nextIteratorWithSmallestValue.setCurrent(): iterators.remove(nextIteratorWithSmallestValue)
+        
+def sortFile(fileName):
+    dataToWrite = sorted(FileIO.iterateJsonFromFile(fileName), key=lambda l: dateutil.parser.parse(l['CreationDate']))
+    print 'Sorting file', fileName
+    f = open('%s_sorted'%fileName, 'w')
+    for line in dataToWrite: f.write(cjson.encode(line)+'\n')
+    os.system('mv %s_sorted %s'%(fileName, fileName))
+    f.close()
+
+def createOutputFileFor(current_data_path, iterators):
+    numberOfSplit = 10
+    totalLines = 0
+    tempDir = '%s/temp/'%current_data_path
+    allDataFile = tempDir+'data.txt'
+    if not os.path.exists(tempDir):  os.umask(0), os.makedirs(tempDir, 0777)
+    for iterator in iterators:
+        for data in iterator: 
+            totalLines+=1
+            FileIO.writeToFileAsJson(data, allDataFile)
+    linesPerFile = totalLines/numberOfSplit
+    os.chdir(tempDir)
+    os.system('split -l %s %s'%(linesPerFile, allDataFile))
+
+    [sortFile(file) for file in glob.glob( os.path.join(tempDir, '*') ) if file!=allDataFile]
+    
+    i = 1
+    for data in iterateDataOrderedByTime([FileIO.iterateJsonFromFile(file) for file in glob.glob( os.path.join(tempDir, '*')) if file!=allDataFile]):
+        print i, data['RowType'], data['CreationDate']
+        i+=1
 
 createOutputFileFor(current_data_path, [iteratePosts(current_data_path), iterateComments(current_data_path)])
+
+#sortFile('/mnt/chevron/kykamath/data/stack_exchange/Content/android_enthusiasts/temp/xaa')
 
 #i = 1
 #for data in iterateDataOrderedByTime([Iterator(iterateComments(current_data_path)), Iterator(iteratePosts(current_data_path))]):
