2b75132c7b7f2cc9dc9520172bd5887e79dc7315
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-29 13:32:01 -0500

    added code to parse comments

--- parser.py
+++ parser.py
@@ -12,17 +12,22 @@ def iterateRows(file, interstedKeys=[]):
     for line in open(file):
         if line.strip().startswith('<row'): 
             dataToYield = dict([(k, etree.fromstring(line).attrib[k]) for k in interstedKeys if k in etree.fromstring(line).attrib])
-            if 'CreationDate' in dataToYield: dataToYield['CreationDate'] =  dateutil.parser.parse(dataToYield['CreationDate'])
+            for k in dataToYield:
+                if k=='CreationDate': dataToYield[k] =  dateutil.parser.parse(dataToYield[k])
+                elif k.endswith('Count') or k.endswith('Id'): 
+                    if dataToYield[k]!='':dataToYield[k]=int(dataToYield[k])
+                    else: dataToYield[k]=0
+                elif k=='Tags': dataToYield[k] = dataToYield[k][1:-1].split('><')
             yield dataToYield
             
 def iteratePosts(data_path, interstedKeys = 'Id PostTypeId ParentID CreationDate ViewCount OwnerUserId Tags AnswerCount CommentCount FavoriteCount'.split()):
     for l in iterateRows(data_path+'posts.xml', interstedKeys): yield l
+def iterateComments(data_path, interstedKeys = 'Id PostId CreationDate UserId'.split()):
+    for l in iterateRows(data_path+'comments.xml', interstedKeys): yield l
 
 i = 1
-for l in iteratePosts(current_data_path):
+for l in iterateComments(current_data_path):
     print i, l
     i+=1
-#import dateutil.parser
-#s='2008-09-06T08:07:10.730'
-#print type(dateutil.parser.parse(s))
+
     
